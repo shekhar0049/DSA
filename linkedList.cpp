04b8f6a0154cffd2fed27d59ff38fd558633768a
Raj Shekhar <rajshekharsingh@RAJSHEKHARs-MacBook-Air.local>
2025-09-05 03:20:29 +0530

    conversion to LL

--- linkedList.cpp
+++ linkedList.cpp
@@ -14,6 +14,7 @@ struct Node{
     }
 
     public:
+
     Node(int data1){
         data = data1;
         next = nullptr;
@@ -21,11 +22,52 @@ struct Node{
     }
 
 };
+Node* arraytoLL(vector<int>&arr){
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
 
+    for(int i=1; i<arr.size(); i++){
+           Node* temp = new Node(arr[i]);
+           mover->next = temp;
+           mover = temp;
+        
+    }
+    return head;
+}
+
+int lengthofLL(Node* head){
+    int count = 0; 
+    Node* temp = head;
+    while(temp){
+        count++;
+        temp = temp->next;
+    }
+    return count;
+
+}
+// bool searchinLL(Node* head, int key){
+//     Node* temp = head;
+//     while(temp){
+//         if(temp->data == key){
+//             return 1;
+//         }
+//     }
+//     return 0;
+//}
 int main(){
-    vector<int> arr = {0,1,2,3};
-    Node* y = new Node(arr[3]);
+    vector<int> arr = {15,1,2,3};
+    int count;
+    Node* head = arraytoLL(arr);
+    Node* temp = head;
 
-    cout<< y->data << endl;
+    while(temp){
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+//     bool ans;
+//  ans =searchinLL(head, 2);
+    count = lengthofLL(head);
+    cout << endl << count << endl;
+    // cout << endl << ans << endl;
 
 }

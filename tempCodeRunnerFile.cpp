36675bd3130d8888ee5e390297168206c34b2dc7
Raj Shekhar <rajshekharsingh@RAJSHEKHARs-MacBook-Air.local>
2025-09-05 03:22:05 +0530

    temp file

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,16 +1,2 @@
-int n;
-char ch ;
-cin>> n;
-cout << "what do u want sum of :";
-cin >> ch;
-int sum= 0;
- if(ch == 'e'){
-
-   int i= 2;
-   while (i<=n){
-   sum = sum + i;
-   i= i+2; 
-    
-   } 
-   cout << sum << endl;
- } 
+ bool ans;
+//  ans =searchinLL(head, 2)

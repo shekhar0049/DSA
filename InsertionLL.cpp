26596fd0ac11b374cfcd33ee7cf6cc04b74164b2
Raj Shekhar <rajshekharsingh@RAJSHEKHARs-MacBook-Air.local>
2025-09-09 01:52:29 +0530

    Insertion in LL

--- InsertionLL.cpp
+++ InsertionLL.cpp
@@ -18,6 +18,7 @@ struct Node{
     }
 };
 Node* arraytoLL(vector<int>&arr){
+    if(arr.empty())return nullptr;
     Node* head = new Node(arr[0]);
     Node* mover = head;
 
@@ -36,28 +37,80 @@ void print(Node* head){
     }
 }
 
-Node* InsertHead(Node* head, int val){
-    Node* temp = new Node(val, head);
-    return temp;
-}
+// Node* InsertHead(Node* head, int val){
+//     Node* temp = new Node(val, head);
+//     return temp;
+// }
 
 // Node* InsertLast(Node*head , int val){
-//     Node* temp = head;
-//     while(temp != NULL){
-//         if(temp->next == NULL){
-//             Node* last = new Node(val);
-//             temp-> next = last;
-//         }
+//     if(head == NULL){
+//         return new Node(val);
 //     }
+//     Node* temp = head;
+//     while(temp->next != NULL){
+//         temp = temp->next;
+//     }
+//     Node* last = new Node(val);
+//     temp->next = last;
 //     return head;
     
 // }
+// Node* InsertAtPos(Node* head, int k , int val){
+//     if(head == NULL){
+//         if(k == 1){
+//             return new Node(val);
+//         }
+//         else{
+//             return NULL;
+//         }
+//     }
+//     if(k == 1){
+//         Node* temp = new Node(val, head);
+//         return temp;
+//     }
+//     int cnt = 0;
+//     Node* temp = head;
+//     while(temp){
+//         cnt++;
+//         if(cnt == k-1){
+//             Node* x = new Node(val);
+//             x->next = temp->next;
+//             temp->next = x;
+//             break;
+//         }
+//         temp = temp->next;
+//     }
+//     return head;
+// }
+Node* InsertBeforeVal(Node* head, int el , int val){
+    if(head == NULL){
+        return  NULL;
+    }
+    if(head->data == val){
+        Node* temp = new Node(el, head);
+        return temp;
+    }
+
+    Node* temp = head;
+    while(temp->next != NULL){
+        
+        if(temp->next->data == val){
+            Node* x = new Node(el,temp->next);
+            temp->next = x;
+            break;
+        }
+        temp = temp->next;
+    }
+    return head;
+}
 
 int main(){
-    vector<int> arr = {1,4,7,2,5};
+    vector<int> arr = {46, 32, 52, 73, 22};
     Node* head = arraytoLL(arr);
-    head = InsertHead(head, 28);
+    // head = InsertHead(head, 28);
     // head = InsertLast(head, 35);
+    // head = InsertAtPos(head, 1, 35);
+    head = InsertBeforeVal(head, 34, 22);
     print(head);
 
 }

d741d9df5a25edea2ff6adbdad327892d7936e14
shekhar_0049 <144986095+shekhar0049@users.noreply.github.com>
2025-09-09 23:07:19 +0530

    Time: 20 ms (14.36%) | Memory: 9.4 MB (53.22%) - LeetSync

---
+++ 2408-number-of-people-aware-of-a-secret/number-of-people-aware-of-a-secret.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int M = 1e9 + 7;
+    vector<int> t;
+
+    int solve(int day, int delay, int forget){
+        if(day == 1){
+            return 1;
+        }
+        if(t[day] != -1){
+            return t[day];
+        }
+
+        long long result = 0;
+        for(int prev = day-forget+1; prev <= day-delay; prev++){
+            if(prev>0){
+                result = (result + solve(prev, delay, forget)) % M;
+
+            }
+        }
+        return t[day] = result;
+    }
+    int peopleAwareOfSecret(int n, int delay, int forget) {
+        long long total = 0;
+        t.assign(n+1, -1);
+        for(int day = n-forget+1; day <= n; day++){
+            if(day>0){
+                total = (total + solve(day, delay, forget)) % M;
+
+            }
+
+
+        }
+        return total;
+    }
+};

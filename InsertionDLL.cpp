c9a2be0249670595863891ad25eb8191f888627f
Raj Shekhar <rajshekharsingh@RAJSHEKHARs-MacBook-Air.local>
2025-09-11 00:16:42 +0530

    Insertion in Dll

---
+++ InsertionDLL.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+
+struct Node{
+    public:
+    int data;
+    Node* next;
+    Node* back;
+
+    public:
+    Node(int data1, Node* next1, Node* back1){
+        data = data1;
+        next = next1;
+        back = back1;
+    }
+
+    public:
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+        back = nullptr;
+    }
+
+};
+
+Node* arr2dll(vector<int>arr){
+    if(arr.empty())return NULL;
+
+    Node* head = new Node(arr[0]);
+    Node* prev = head;
+
+    for(int i = 1 ; i<arr.size(); i++){
+        Node* temp = new Node(arr[i], nullptr, prev);
+        prev->next = temp;
+        prev = temp;
+    }
+    return head;
+}
+void print(Node *head){
+    while(head != NULL){
+        cout<< head->data << " ";
+        head = head->next;
+    }
+}
+
+int main(){
+    vector<int> arr = {3,8,1,0,2};
+    Node* head = arr2dll(arr);
+    print(head);
+
+return 0;
+}

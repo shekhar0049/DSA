679f64832a5877b0fab1db5508cafb689a891c53
Raj Shekhar <rajshekharsingh@RAJSHEKHARs-MacBook-Air.local>
2025-09-10 23:59:55 +0530

    Deleteion DLL final

--- DeletionDLL.cpp
+++ DeletionDLL.cpp
@@ -49,26 +49,74 @@ void print(Node* head){
 //     return head;
 // }
 
-Node* DeleteTail(Node* head){
-    if(head == NULL || head->next == NULL)return NULL;
-    Node* temp = head;
+// Node* DeleteTail(Node* head){
+//     if(head == NULL || head->next == NULL)return NULL;
+//     Node* temp = head;
     
-    while(temp){
-        if(temp->next == NULL){
-            Node* prev = temp->back;
-            temp->back = NULL;
-            prev->next = NULL;
-            free(temp);
-        }
-        temp = temp->next;
+//     while(temp){
+//         if(temp->next == NULL){
+//             Node* prev = temp->back;
+//             temp->back = NULL;
+//             prev->next = NULL;
+//             free(temp);
+//         }
+//         temp = temp->next;
+//     }
+//     return head;
+// }
+
+// Node* DeleteKth(Node* head , int k){
+//     if(head == NULL)return NULL;
+//     Node* temp = head;
+//     int cnt = 0;
+//     while(temp){
+//        cnt ++;
+//        if(cnt == k)break;
+//        temp = temp->next;   
+//     }
+//     Node* prev = temp->back;
+//     Node* Next = temp->next;
+
+//     if(prev == NULL && Next == NULL){
+//         return NULL;
+//     }
+//     else if(prev == NULL){
+//         return DeleteHead(head);
+//     }
+//     else if(Next == NULL){
+//         return DeleteTail(head);
+//     }
+//     prev->next = Next;
+//     Next->back = prev;
+//     temp->next = nullptr;
+//     temp->back = nullptr;
+//     free(temp);
+//     return head;
+// }
+
+void DeleteNode(Node* temp){
+    Node* prev = temp->back;
+    Node* Next = temp->next;
+
+    if(Next == NULL){
+        prev->next = nullptr;
+        temp->back = nullptr;
+        free(temp);
+        return;
     }
-    return head;
+    prev->next = Next;
+    Next->back = prev;
+    temp->next = temp->back = nullptr;
+    free(temp);
+
 }
 int main(){
-    vector<int> arr ={1,2};
+    vector<int> arr ={3,7,1,0,9};
     Node* head = arr2dll(arr);
     // head = DeleteHead(head); 
-    head = DeleteTail(head);
+    // head = DeleteTail(head);
+    // head = DeleteKth(head, 1);
+    DeleteNode(head->next->next);
     print(head);
 
 }

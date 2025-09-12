e9f30d34813a98ffef7955308da3857ec934ab28
Raj Shekhar <rajshekharsingh@RAJSHEKHARs-MacBook-Air.local>
2025-09-12 09:48:44 +0530

    Insertion in DLL

--- InsertionDLL.cpp
+++ InsertionDLL.cpp
@@ -43,9 +43,50 @@ void print(Node *head){
     }
 }
 
+Node* insertBeforeHead(Node* head, int val){
+    if(head == NULL)return new Node(val, nullptr, nullptr);
+    Node* newHead = new Node(val,head,nullptr);
+    head->back = newHead;
+    return newHead;
+}
+Node* insertBeforeTail(Node* head, int val){
+    if(head == NULL)return new Node(val, nullptr, nullptr);
+    if(head->next == NULL){
+      Node* tail = new Node(val, head, nullptr);
+      head->back = tail;
+      return tail;
+    }
+    Node* temp = head;
+    while(temp->next->next != NULL){
+        temp = temp->next;
+    }
+    Node* tail = new Node(val, temp->next, temp);
+    temp->next = tail;
+    tail->next->back = tail;
+    return head;
+
+}
+
+Node* insertBeforeKth(Node* head, int k, int val){
+    if(k==1)return insertBeforeHead(head, val);
+    Node* temp = head;
+    int cnt = 0;
+    while(temp){
+        cnt++;
+        if(cnt == k)break;
+        temp = temp->next;
+    }
+    Node* prev = temp->back;
+    Node* Kth = new Node(val, temp, prev);
+    prev->next = temp->back = Kth;
+    return head;
+
+}
+
 int main(){
-    vector<int> arr = {3,8,1,0,2};
+    vector<int> arr = {5,8,12,4,3};
     Node* head = arr2dll(arr);
+    head = insertBeforeKth(head , 5, 22);
     print(head);
 
 return 0;

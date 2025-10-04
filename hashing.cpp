763e0c0e60003428c37846b49e798e4b96381e8b
Raj Shekhar <rajshekharsingh@RAJSHEKHARs-MacBook-Air.local>
2025-10-04 15:56:10 +0530

    hashing with char

--- hashing.cpp
+++ hashing.cpp
@@ -1,34 +1,59 @@
 # include<iostream>
+#include<string>
 using namespace std;
 
 int main(){
-    // hashing with numbers
+    // // hashing with numbers
 
-    int n;
-    cin >> n;
-    int arr[n];
+    // int n;
+    // cin >> n;
+    // int arr[n];
 
-    for(int i = 0; i<n ; i++){
-        cin >> arr[i];
-    }
-    int hash[50] = {0};
+    // for(int i = 0; i<n ; i++){
+    //     cin >> arr[i];
+    // }
+    // int hash[50] = {0};
 
-    //pre-compute
+    // //pre-compute
 
-    for(int i = 0; i<n ; i++){
-        hash[arr[i]]++;
+    // for(int i = 0; i<n ; i++){
+    //     hash[arr[i]]++;
+    // }
+    // int q;
+    // cin >> q;
+    // int qrr[q];
+
+    // for(int j =0; j<q; j++){
+    //     cin >> qrr[j];
+    // }
+
+    // for(int i = 0; i < q ; i++){
+    //     cout << hash[qrr[i]] << endl;
+    // }
+
+
+
+    // hashing with characters;
+
+    string s = "achfjrgjekdn";
+
+    int hash[26] = {0};
+
+    for(int i = 0; i<s.size(); i++){
+        hash[s[i] - 'a']++;
     }
     int q;
     cin >> q;
-    int qrr[q];
-
-    for(int j =0; j<q; j++){
-        cin >> qrr[j];
+    char qrr[q];
+    
+    for(int i= 0; i< q; i++){
+        cin >> qrr[i];
+    }
+    for(int j = 0; j < q; j++){
+        cout << hash[qrr[j] - 'a'] << "\t";
     }
 
-    for(int i = 0; i < q ; i++){
-        cout << hash[qrr[i]] << endl;
-    }
+   
 
     return 0;
 }
